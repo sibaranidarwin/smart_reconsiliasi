c0f1b3010c46709c6996cf6e4d26bca609eccd7b
Darwin Sibarani <darwin.sibarani@ist.id>
2025-05-07 14:36:09 +0700

    Add depedency jwt token dan hibernate

---
+++ smart_reconsiliasi/src/main/java/com/hackathon/smart_reconsiliasi/SmartReconsiliasiApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.smart_reconsiliasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReconsiliasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartReconsiliasiApplication.class, args);
+	}
+
+}
